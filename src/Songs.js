a116438924da6e5bd98e122b92ceae4db0be452f
Sahine Diallo <psdiallo300@gmail.com>
2022-05-10 21:05:50 +0000

    finish rendering the songs

--- src/Songs.js
+++ src/Songs.js
@@ -2,6 +2,17 @@ import React from 'react'
 import './Songs.css';
 
 function Songs({ track }) {
+    const convertMinutes = (duration)=> {
+        var seconds = Math.floor((duration / 1000) % 60);
+        var minutes = Math.floor((duration / (1000 * 60)) % 60);
+        var hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+      
+        hours = (hours < 10) ? "0" + hours : hours;
+        minutes = (minutes < 10) ? "0" + minutes : minutes;
+        seconds = (seconds < 10) ? "0" + seconds : seconds;
+      
+        return  minutes + ":" + seconds ;
+    }
   return (
     <div className="songs">
 
@@ -14,10 +25,10 @@ function Songs({ track }) {
             </div>
         </div>
         <div className="song__album">
-
+            <p>{track?.track?.album?.name}</p>
         </div>
         <div className="song__duration">
-
+            <p>{convertMinutes(`${track?.track?.duration_ms}`)}</p>
         </div>
     </div>
   )
